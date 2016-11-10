4b413229af1ed576262b0b56584f3d7895cbedf3
ABigBigWhale <chakalakafrank@gmail.com>
2016-11-10 10:17:54 -0800

    Adding animations

--- js/Card.jsx
+++ js/Card.jsx
@@ -1,12 +1,23 @@
 class Card extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {showContent: false}
         
         this.handleClick = this.handleClick.bind(this);
+        this.handleMouseEnter = this.handleMouseEnter.bind(this);
+        this.handleMouseLeave = this.handleMouseLeave.bind(this);
     }
     
     handleClick() {
         console.log('id: ' + this.props.snapshotId);
+    }
+    
+    handleMouseEnter() {
+        this.setState({showContent: true});
+    }
+    
+    handleMouseLeave() {
+        this.setState({showContent: false});
     }
 
     render() {
@@ -23,10 +34,18 @@ class Card extends React.Component {
                         <div className="card-image">
                             <img src={this.props.imgsrc} />
                         </div>
-                        <div className="card-stacked">
+                        <div className="card-stacked"
+                            onMouseEnter={this.handleMouseEnter}
+                                    onMouseLeave={this.handleMouseLeave}>
+                            
+                            {this.state.showContent ? <HoverContent /> : null}
+                            
                             <div className="card-content">
                                 <p>{this.props.text}</p>
                             </div>
+                            
+                            
+                            
                             <div className="card-action">
                                 <a className='card-link' href="#" onClick={this.handleClick}>Start</a>
                             </div>
@@ -35,5 +54,25 @@ class Card extends React.Component {
                 </div>
             </TransitionGroup>
         );
+    }
+}
+
+class HoverContent extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return (
+            <TransitionGroup
+                transitionName="fade-in"
+                transitionAppear={true}
+                transitionAppearTimeout={500}
+                transitionEnterTimeout={500}
+                transitionLeaveTimeout={300}>
+
+                <div ref='hoverContent' className="card-content-hover"></div>
+            </TransitionGroup>
+            );
     }
 }
